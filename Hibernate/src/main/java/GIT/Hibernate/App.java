e93b08d7a4866661ffab3e95e20867fc0cf42474
Murtiza Khursheed <154056587+Murtiza007@users.noreply.github.com>
2025-08-12 15:14:20 +0530

    Add files via upload

--- Hibernate/src/main/java/GIT/Hibernate/App.java
+++ Hibernate/src/main/java/GIT/Hibernate/App.java
@@ -1,9 +1,12 @@
 package GIT.Hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import GIT.Hibernate.entity.Student;
 
@@ -11,25 +14,67 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        Student std=new Student();
-        std.setId(104);
-        std.setName("musa");
-        std.setEmail("momin@");
-        std.setBatch("java");
+        //Student std=new Student();
         
+        //insert to hibernate
+        
+//        std.setId(103);
+//        std.setName("basim");
+//        std.setEmail("momin@");
+//        std.setBatch("java");
+//        
         Configuration cfg = new Configuration();
-        cfg.configure("configuration/hibernate.cfg.xml"); 
+       cfg.configure("configuration/hibernate.cfg.xml"); 
 
-       
-        SessionFactory sessionFactory = cfg.buildSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+    
+      SessionFactory sessionFactory = cfg.buildSessionFactory();
+       Session session = sessionFactory.openSession();
+      Transaction transaction = session.beginTransaction();
+//
+//        session.save(std);
+//        transaction.commit();
+//
+//  
+//        session.close();
+//        sessionFactory.close();
+//        
+      
+      	//retrieve
+//        Student std=session.get(Student.class, 102);
+//        
+//        System.out.println(std.getName());
+//        System.out.println(std.getBatch());
+//        System.out.println(std.getEmail());
+//        
+       //update
+//      Student std=session.get(Student.class, 101);
+//      std.setBatch("spring");
+//      session.saveOrUpdate(std);
+//      transaction.commit();
+//      System.out.println("work done");
+//    
+      //delete
+      //Student std=session.get(Student.class, 101);
+      
+//      Student std =new Student();
+//      std.setId(102);
+//      session.delete(std);
+//      transaction.commit();
+//      System.out.println("work done");
 
-        session.save(std);
-        transaction.commit();
+      
+      //display all
+      
+      Query<Student> query = session.createQuery("FROM Student", Student.class);
+      List<Student> students = query.list();
 
-  
-        session.close();
-        sessionFactory.close();
+      for (Student s : students) {
+          System.out.println(s.getId() + " - " + s.getName()+" - "+s.getBatch());
+      }
+
+      transaction.commit();
+      session.close();
+        
+        
     }
 }
